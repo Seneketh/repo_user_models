b8350ba7389cb52a390a0c8a6bf36cb44a12a5d1
Marco Wirthlin <marco.wirthlin@gmail.com>
2016-12-04 21:34:26 +0100

    proper dying implemented

--- endlessrunner.py
+++ endlessrunner.py
@@ -3,6 +3,7 @@ from Obstacle import *
 from gamedisplay import *
 from numpy import *
 import pygame
+import time
 from random import randint;
 
 screenSize = (800, 800)
@@ -21,7 +22,7 @@ pygame.display.set_caption('Endlessrunner of Doom')
 # variables for controlling the loop
 gameExit = False
 gameQuit = False
-
+gameLevel = 0
 # objexts that are in play
 
 playerbody = PlayerCube(playersize_x, playersize_y, player_speed, screenSize[0]*0.5, screenSize[1]*0.8, screenSize)
@@ -31,9 +32,24 @@ obstacleHandler = ObstacleList(10,screenSize, 10, screenSize[1]/8 ,screenSize[0]
 
 # cubex/8 and cubey/40 always result in obstacles that have a grid with 20 lanes in y and 40 lanes in x if resolution is div by 2
 
+def text_objects(text, TextConf):
+    TextSurface = pygame.font.Font.render(TextConf, text, True, black)
+    return TextSurface, TextSurface.get_rect()
+
+def message_display(text):
+    TextConf = pygame.font.Font('freesansbold.ttf', 90)
+    TextSurface, TextRectangle = text_objects(text, TextConf)
+    TextRectangle.center = (screenSize[0]/2, screenSize[1]/2)
+    game_display.blit(TextSurface, TextRectangle) #display it
+    pygame.display.flip()
+    time.sleep(2)
+
+def player_death():
+    message_display('You died horribly')
 
 while not gameExit: # outer loop for quitting
     movement = 0
+
     obstacleHandler.restart()
 
     while not gameQuit: #inner  loop for restarting the game
@@ -49,7 +65,6 @@ while not gameExit: # outer loop for quitting
                 if event.key == pygame.K_a:
                     movement = 0
 
-
             if event.type == pygame.QUIT:
                 gameExit = True
                 gameQuit = True
@@ -59,6 +74,7 @@ while not gameExit: # outer loop for quitting
         if collision:
             movement = 0
             obstacleHandler.restart()
+            player_death()
 
         obstacleHandler.update()
         playerbody.changePosition(movement)
@@ -66,10 +82,6 @@ while not gameExit: # outer loop for quitting
         # graphics call
         updateScreen(game_display, playerbody, obstacleHandler.obstacles)
 
-
-
         # updating the display and wating for frame rate
         pygame.display.flip()
         clock.tick(60)
-
-#MAKING DIFFICULT HIGHER BY BLOCK COLOR = BACKGROUND
